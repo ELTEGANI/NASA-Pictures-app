f85156fe1427c161ee6ba7e3b37fd164ed556be6
altegani mohammed <tiganimoh@gmail.com>
2020-11-23 16:18:39 +0200

    add daggerHilt setup to the whole app

--- app/src/main/java/com/example/nasapicturesapp/imagesdetailes/ImageDetailScreenFragment.kt
+++ app/src/main/java/com/example/nasapicturesapp/imagesdetailes/ImageDetailScreenFragment.kt
@@ -1,32 +1,34 @@
 package com.example.nasapicturesapp.imagesdetailes
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.example.nasapicturesapp.R
+import com.example.nasapicturesapp.databinding.ImageDetailScreenFragmentBinding
+import com.example.nasapicturesapp.databinding.ImagesGridFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class ImageDetailScreenFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ImageDetailScreenFragment()
+
+    private lateinit var imageDetailScreenFragmentBinding: ImageDetailScreenFragmentBinding
+    private val imageDetailScreenViewModel:ImageDetailScreenViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        imageDetailScreenFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.image_detail_screen_fragment, container, false)
+
+        imageDetailScreenFragmentBinding.lifecycleOwner = this
+        imageDetailScreenFragmentBinding.imageDetailesScreen = imageDetailScreenViewModel
+
+        return imageDetailScreenFragmentBinding.root
     }
 
-    private lateinit var viewModel: ImageDetailScreenViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.image_detail_screen_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ImageDetailScreenViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
--- app/src/main/java/com/example/nasapicturesapp/imagesdetailes/ImageDetailScreenViewModel.kt
+++ app/src/main/java/com/example/nasapicturesapp/imagesdetailes/ImageDetailScreenViewModel.kt
@@ -1,7 +1,8 @@
 package com.example.nasapicturesapp.imagesdetailes
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 
-class ImageDetailScreenViewModel : ViewModel() {
+class ImageDetailScreenViewModel @ViewModelInject constructor(): ViewModel() {
     // TODO: Implement the ViewModel
 }
