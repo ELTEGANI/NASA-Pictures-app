f5da89289ffdc50e1552069f1e3a2f4e4b80e09f
altegani mohammed <tiganimoh@gmail.com>
2020-11-27 21:49:16 +0200

    add image with its detailes to deatiles screen with ability to swipe

--- app/src/main/java/com/example/nasapicturesapp/imagesdetailes/ImageDetailScreenViewModel.kt
+++ app/src/main/java/com/example/nasapicturesapp/imagesdetailes/ImageDetailScreenViewModel.kt
@@ -2,7 +2,8 @@ package com.example.nasapicturesapp.imagesdetailes
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.example.nasapicturesapp.data.ImagesProperties
 
 class ImageDetailScreenViewModel @ViewModelInject constructor(): ViewModel() {
-    
+
 }
